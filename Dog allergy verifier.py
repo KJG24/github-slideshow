bd2038b7172242c4611c6a557e976cf66a8fcd47
KJG24 <78676376+KJG24@users.noreply.github.com>
2021-02-07 22:18:34 -0800

    Add files via upload

---
+++ Dog allergy verifier.py
@@ -0,0 +1,10 @@
+user_input = input("What did your dog eat?")
+
+if user_input == "chocolate" :
+  print("Toxic: side effects of ingesting chocolate")
+elif user_input == "chicken" :
+  print("Not Harmful")
+elif user_input == "carrots" :
+  print("Not Harmful")
+  
+
