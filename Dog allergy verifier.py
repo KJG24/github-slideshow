903bbc796f1efebb866a22c36c680335ce819ac0
KJG24 <78676376+KJG24@users.noreply.github.com>
2021-02-08 16:41:40 -0800

    Add files via upload

--- Dog allergy verifier.py
+++ Dog allergy verifier.py
@@ -1,10 +1,26 @@
-user_input = input("What did your dog eat?")
+user_input = input("What did your dog eat?").lower()
+
 
 if user_input == "chocolate" :
-  print("Toxic: side effects of ingesting chocolate")
-elif user_input == "chicken" :
-  print("Not Harmful")
+  print("Toxic: The darker, more bitter, and more concentrated the chocolate is, the more dangerous it is. Please see your local veterinarian.")
 elif user_input == "carrots" :
   print("Not Harmful")
-  
+elif user_input == "xylitol" :
+  print("Toxic: Many sugarless gums and candies contain xylitol, a sweetener that is dangerous to dogs. When ingested, even in small amounts, it can result in a life-threatening drop in blood sugar or even liver failure. Please see your local veterinarian.")
+elif user_input == "gum" :
+  print("Toxic: Many sugarless gums and candies contain xylitol, a sweetener that is dangerous to dogs. When ingested, even in small amounts, it can result in a life-threatening drop in blood sugar or even liver failure. Please see your local veterinarian.")
+elif user_input == "grapes" :
+  print("Toxic: Raisins and grapes are often overlooked as one of the most toxic foods to dogs, and can result in kidney failure. Please see your local veterinarian.")
+elif user_input == "raisins" :
+  print("Toxic: Raisins and grapes are often overlooked as one of the most toxic foods to dogs, and can result in kidney failure. Please see your local veterinarian.")
+else :
+  print("Hmmm it looks like we don't have that in our database, please consult your local veterinarian.")
+
+
+
+
+
+
+
+
 
